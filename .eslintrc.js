ecac5d235a0fdbb7c0af7871d842daa35d36e792
pillarliang <pillarliang@tencent.com>
2021-09-12 15:52:21 +0800

    feat(架构): 环境搭建

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,11 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        'prettier',
+        'prettier/@typescript-eslint',
+        'prettier/react',
+        'prettier/unicorn',
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
